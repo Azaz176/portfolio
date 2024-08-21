369472f736c65d3523995e45719fe7c3b46b486c
Azaz <azaz.ali1706@gmail.com>
2024-08-21 18:23:49 +0530

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,407 @@
+import my_profile_pic from '../assests/images/my_pic.png';
+import myPic from '../assests/images/my_pic.png';
+import blood_img from '../assests/images/blood.png';
+import property_img from '../assests/images/property.png';
+import hire_img from '../assests/images/hirehu.png';
+import video_img from '../assests/images/video.png';
+import basket_img from '../assests/images/basketball.png';
+import guess_img from '../assests/images/guessnumber.png';
+
+export const Bio = {
+  name: "Azaz Ali",
+  roles: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "Problem Solver",
+  ],
+  description:
+    "Explore the work of Azaz, a passionate developer with a strong foundation in Computer Science and a commitment to creating efficient, scalable software solutions. With expertise in full-stack development and software engineering, skilled in both front-end and back-end technologies, ensuring seamless user experiences and robust backend systems..",
+  github: "https://github.com/Azaz176",
+  resume:
+    "/",
+  linkedin: "https://www.linkedin.com/in/azaz-ali-8541a5241/",
+  twitter: "https://x.com/Azaz_Dev",
+  github: "https://github.com/Azaz176",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Next Js",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1400/1*BQZAbczBfLYtPp-6HmN0ZQ.jpeg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png?20221110153201",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSDKn3vA2YUbXzN0ZC3gALWJ08gJN-Drl15w&s",
+      },
+      {
+        name: "SCSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/768px-Sass_Logo_Color.svg.png?20150315202757",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "NodeJS",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-512/free-node-js-logo-icon-download-in-svg-png-gif-file-formats--nodejs-programming-language-pack-logos-icons-1174925.png?f=webp&w=256",
+      },
+      {
+        name: "Express",
+        image: "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo-thumbnail.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://www.svgrepo.com/show/331488/mongodb.svg",
+      },
+      {
+        name: "PostgreSQL",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MySQL",
+        image: "https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png",
+      },
+      {
+        name: "Redis",
+        image: "https://p7.hiclipart.com/preview/326/224/1009/redis-memcached-database-caching-key-value-database-others-thumbnail.jpg",
+      },
+      {
+        name: "Rest APIs",
+        image: "https://media.licdn.com/dms/image/v2/D4D12AQEeNNHq05k7MA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1687786979245?e=1729728000&v=beta&t=mXPDUSKrIAmB_BclO0fNohTzAIucbO6RYBRuGSTTWxM",
+      },
+      {
+        name: "Socket IO",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Socket-io.svg/900px-Socket-io.svg.png?20200308235956",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Docker",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/ea/Docker_%28container_engine%29_logo_%28cropped%29.png?20230309095311",
+      },
+      {
+        name: "Git",
+        image:
+          "https://book.git-scm.com/images/logos/logomark-orange@2x.png",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/dd/Linux_logo.jpg?20090908114920",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.svgrepo.com/show/354202/postman-icon.svg",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/768px-Amazon_Web_Services_Logo.svg.png?20170912170050",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/459px-ISO_C%2B%2B_Logo.svg.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/172px-Python-logo-notext.svg.png",
+      },
+      {
+        name: "DBMS",
+        image:
+          "https://w7.pngwing.com/pngs/931/769/png-transparent-database-icon-database-free-blue-background-blue-angle-world-thumbnail.png",
+      },
+      {
+        name: "Computer Networks",
+        image:
+          "https://cdn.vectorstock.com/i/1000v/02/30/computer-networks-earth-globe-vector-18440230.avif",
+      },
+      {
+        name: "Data Structures",
+        image:
+          "https://play-lh.googleusercontent.com/a4Xrc-8oQLu05mOrNPuvA_o2nZEIEnOoTH4wB91Slw_hCvuIu_Qgi440bK9mC8ml-KA=w240-h480-rw",
+      },
+      
+      {
+        name: "Algorithms",
+        image:
+          "https://c7.alamy.com/comp/R1PYDD/algorithm-vector-icon-isolated-on-transparent-background-algorithm-transparency-logo-concept-R1PYDD.jpg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.4techbugs.com/assets/images/4tblogo.png",
+    role: "Backend Developer Intern",
+    company: "4techbugs",
+    date: "May 2024 - July 2024",
+    desc: "Developed and maintained RESTful APIs using Node.js,  Integrated APIs with front-end interfaces through Axios.",
+    skills: [
+      "NodeJS",
+      "Express",
+      "JS",
+      "Postman",
+      "Docker",
+      "Rest APIs",
+      "MongoDB"
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "https://www.iiitd.ac.in/sites/default/files/images/logo/style1colorlarge.jpg",
+    role: "Summer Research Intern",
+    company: "",
+    date: "July 2023 - Sep 2023",
+    desc: " Developed and implemented machine learning algorithms to detect and mitigate harmful memes on social media platforms",
+    skills: [
+      "Python",
+      "Machine Learning",
+      "Data Analysis"
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "https://avatars.githubusercontent.com/u/95990630?s=200&v=4",
+    role: "Technical Community Manager",
+    company: "LearnWeb3.io",
+    date: "Jul 2022 - Dec 2022",
+    desc: "Led efforts to grow and engage technical communities through content creation, events, and discussions.", 
+    skills: [
+      "Discord",
+      "Web3",
+      "Organization",
+      "Management",
+      "BlockChain Dev"
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://alumni.mait.ac.in/images/MAIT/Mait_Logo.png",
+    school: "Maharaja Agrasen Institute of Technology",
+    date: "2020 - 2024",
+    grade: "9.3 CGPA",
+    desc: "I have completed my Bachelors in Computer Engineering at Maharaja Agrasen Institute of Technology. I have taken courses in Software Engineering, Machine Learning, and Computer Science. I was also a member of the Google Developer Student Club, where I learned additional technical and soft skills.",
+    degree: "Bachelor of Technology - BTech, Computer Engineering(ECE)",
+  },
+  {
+    id: 1,
+    img: "https://delhischoolsdirectory.com/assets/upload/profileimg/PRFIMGUSR88881664519046.png",
+    school: "Himalaya Public Sr. Sec. School",
+    date: "2018 - 2019",
+    grade: "93.6%",
+    desc: "I completed my class 12 high school education at Himalaya Public Sr. Sec. School, where I studied Mathematics with Computer Science.",
+    degree: "CBSE(XII), Mathematics with Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://delhischoolsdirectory.com/assets/upload/profileimg/PRFIMGUSR88881664519046.png",
+    school: "Himalaya Public Sr. Sec. School",
+    date: "2016 - 2017",
+    grade: "95%",
+    desc: "I completed my class 10 high school education at Himalaya Public Sr. Sec. School, where I studied Mathematics, Science, and Computer Science.",
+    degree: "CBSE(X), Mathematics, Science and Computer Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "VideoTube",
+    description:
+      "Developed a video-sharing platform with 100% coverage of features like uploading, viewing, likes, comments, and playlists.",
+    image: video_img,
+    tags: ["JavaScript", "ReactJS", "NodeJS", "ExpressJS", "MongoDB", "Tailwind CSS"],
+    category: "Web",
+    github: "https://github.com/Azaz176/videoTube",
+    webapp: "https://frontend-delta-lilac-48.vercel.app/",
+    member: [
+      {
+        name: "Azaz",
+        img: myPic,
+        linkedin: "https://www.linkedin.com/in/azaz-ali-8541a5241/",
+        github: "https://github.com/Azaz176",
+      },
+    ],
+  },
+  {
+    id: 0,
+    title: "HireHub",
+    description:
+      " Developed a job portal where recruiters post jobs and candidates search and apply, featuring filtering options.",
+    image: hire_img,
+    tags: ["HTML", "CSS", "Javascript", "NextJS", "ReactJS", "Stripe","MongoDB", "Postman"],
+    category: "Web",
+    github: "https://github.com/Azaz176/HireHub",
+    webapp: "https://hire-hub-five.vercel.app/",
+    member: [
+      {
+        name: "Azaz",
+        img: myPic,
+        linkedin: "https://www.linkedin.com/in/azaz-ali-8541a5241/",
+        github: "https://github.com/Azaz176",
+      },
+    ],
+  },
+  {
+    id: 0,
+    title: "PropertyPal",
+    description:
+      " Developed a real estate website with 100% agent authentication, allowing agents to manage profiles and property listings. Enhanced visitor experience by 40% with advanced search and filter options, including furnished status and parking availability.",
+    image:
+      property_img,
+    tags: ["NodeJS", "ReactJS", "Javascript", "TailwindCSS", "MongoDB", "HTML","Clerk"],
+    category: "Web",
+    github: "https://github.com/Azaz176/PropertyPal",
+    webapp: "https://propertypal-umkn.onrender.com/",
+    member: [
+      {
+        name: "Azaz",
+        img: myPic,
+        linkedin: "https://www.linkedin.com/in/azaz-ali-8541a5241/",
+        github: "https://github.com/Azaz176",
+      },
+    ],
+  },
+  {
+    id: 0,
+    title: "BloodVault",
+    description:
+      " Developed a comprehensive web application for blood donation and distribution, including donor registration, hospital blood requests, and inventory management.  Managed blood inventory and maintained detailed records of donations and hospital transactions",
+    image:
+      blood_img,
+    tags: [
+      "ReactJS",
+      "NodeJS",
+      "JavaScript",
+      "MongoDB",
+      "HTML",
+      "CSS",
+      "Render"
+    ],
+    category: "Web",
+    github: "https://github.com/Azaz176/BloodVault",
+    webapp: "https://blood-vault.onrender.com/login",
+    member: [
+      {
+        name: "Azaz",
+        img: myPic,
+        linkedin: "https://www.linkedin.com/in/azaz-ali-8541a5241/",
+        github: "https://github.com/Azaz176",
+      },
+    ]
+  },
+  {
+    id: 1,
+    title: "Guess the Number",
+    description:
+      `Developed a number-guessing game where users try to guess a randomly generated number within a specified range. The game provides real-time feedback such as "Too Low" or "Too High," adjusting the score based on the number of attempts. Features include score tracking, a reset functionality, and a user-friendly interface inspired by retro video games.`,
+    image:
+      guess_img,
+    tags: ["HTML", "CSS", "Javascript", "Git", "Github", "Deployment", "Netlify"],
+    category: "Web",
+    github: "https://github.com/Azaz176/JavaScript--Projects/tree/main/GuessingNumber",
+    webapp: "https://stupendous-otter-6e04f2.netlify.app/",
+    member: [
+      {
+        name: "Azaz",
+        img: myPic,
+        linkedin: "https://www.linkedin.com/in/azaz-ali-8541a5241/",
+        github: "https://github.com/Azaz176",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Basketball Scorecard",
+    description:
+      "Built a dynamic scorecard application for basketball games, allowing users to update home and guest scores in real-time using simple controls. The app includes functionality to start a new game, end the current game, and record high scores. It provides a clean, user-friendly interface with accurate score tracking for both teams. ",
+    image:
+      basket_img,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "OOPs",
+      "Github",
+      "Git",
+      "Netlify",
+    ],
+    category: "Web",
+    github: "https://github.com/Azaz176/JavaScript--Projects/tree/main/BasketBallScorecardProject",
+    webapp: "https://capable-salamander-9366df.netlify.app/",
+    member: [
+      {
+        name: "Azaz",
+        img: myPic,
+        linkedin: "https://www.linkedin.com/in/azaz-ali-8541a5241/",
+        github: "https://github.com/Azaz176",
+      },
+    ],
+  },
+];
+
+
